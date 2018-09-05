45272a90be9b88673fa0a6721c856a8f426aeea7
扬远 <zhenyang.zzy@antfin.com>
2018-09-05 18:20:06 +0800

    update: load config without app.

--- lib/cli/gen_sdk.ts
+++ lib/cli/gen_sdk.ts
@@ -4,15 +4,12 @@ const utils = require('egg-utils');
 
 export async function genAPISDK(baseDir: string) {
   const framework = utils.getFrameworkPath({ baseDir });
-  const egg = require(framework);
-
-  const app = new egg.Application();
-  const config = app.config.controller;
+  const config = utils.getConfig({ baseDir, framework }).controller;
   let { SDKDir, templatePath, filter, type } = config.genSDK;
-  SDKDir = path.isAbsolute(SDKDir) ? SDKDir : path.join(app.baseDir, SDKDir);
+  SDKDir = path.isAbsolute(SDKDir) ? SDKDir : path.join(baseDir, SDKDir);
   await Promise.all(
     [].concat(config.ctrlDir)
-      .map(dir => path.isAbsolute(dir) ? dir : path.join(app.baseDir, dir))
+      .map(dir => path.isAbsolute(dir) ? dir : path.join(baseDir, dir))
       .map(async dir => genAPISDKByPath(dir, SDKDir, templatePath, type, filter))
   );
   console.log('[egg-controller] gen api sdk.');
