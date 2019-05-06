7b5c17c3683e984d4538b898157c06bc13f2f68a
zhang740 <zhang740@vmzclub.com>
2019-05-06 12:11:40 +0800

    update: split before/after function of transformer.

--- lib/transformer/response-schema/ttsc.ts
+++ lib/transformer/response-schema/ttsc.ts
@@ -30,102 +30,107 @@ export default function transformer(program: ts.Program) {
   return {
     before(ctx: ts.TransformationContext) {
       return (sourceFile: ts.SourceFile) => {
-        // 当前编译文件需要保存的数据
-        const fileData = (METADATA[sourceFile.fileName] = {
-          // 方法定义
-          methodDefine: {},
-          schemaObjects: {},
-        });
-
-        // 遍历文件中所有的 class
-        sourceFile.statements
-          .filter(n => ts.isClassDeclaration(n))
-          .forEach(node => {
-            node.forEachChild(cbNode => {
-              if (isDecoratorNameInclude(cbNode, 'route')) {
-                const clsMethod = getClsMethodKey(
-                  (cbNode.parent as any).symbol.escapedName,
-                  (cbNode as any).symbol.escapedName
-                );
-
-                const type = typeChecker.getTypeAtLocation(cbNode);
-                const returnType = typeChecker.getReturnTypeOfSignature(
-                  type.getCallSignatures()[0]
-                );
-                let realType: SchemaObject;
-                if (getValue(() => returnType.symbol.escapedName) === 'Promise') {
-                  realType = getSchemaByType((returnType as any).typeArguments[0], {
-                    typeChecker,
-                    schemaObjects: fileData.schemaObjects,
-                  });
-                } else {
-                  realType = getSchemaByType(returnType as ts.ObjectType, {
-                    typeChecker,
-                    schemaObjects: fileData.schemaObjects,
-                  });
-                }
-                if (!fileData.methodDefine[clsMethod]) {
-                  fileData.methodDefine[clsMethod] = {
-                    paramSchema: {},
-                    responseSchema: realType,
-                  };
-                }
-              }
-            });
-          });
-
+        before(ctx, sourceFile, typeChecker);
         return walker(sourceFile, ctx);
       };
     },
 
     after(ctx: ts.TransformationContext) {
       return (sourceFile: ts.SourceFile) => {
-        const fileData = METADATA[sourceFile.fileName];
-
-        if (fileData) {
-          sourceFile.statements.forEach(node => {
-            if (ts.isExpressionStatement(node) && (node as any).expression.arguments) {
-              const className = getValue(
-                () => (node as any).expression.arguments[1].expression.escapedText
-              );
-              const methodName = getValue(() => (node as any).expression.arguments[2].text);
-
-              if (className && methodName) {
-                const responseSchema = getValue(
-                  () => fileData.methodDefine[getClsMethodKey(className, methodName)].responseSchema
-                );
-                if (responseSchema) {
-                  (node as any).expression.arguments[0].elements = (node as any).expression.arguments[0].elements.concat(
-                    ts.createCall((ts as any).getHelperName('__metadata'), undefined, [
-                      ts.createLiteral(RESPONSE_SCHEMA_KEY),
-                      convert(responseSchema),
-                    ]),
-                    ts.createCall((ts as any).getHelperName('__metadata'), undefined, [
-                      ts.createLiteral(SCHEMA_DEFINITION_KEY),
-                      ts.createIdentifier('__SchemaDefinition'),
-                    ])
-                  );
-                }
-              }
-            }
-          });
-
-          (sourceFile.statements as any).unshift(
-            ts.createVariableDeclarationList(
-              [
-                ts.createVariableDeclaration(
-                  '__SchemaDefinition',
-                  undefined,
-                  convert(fileData.schemaObjects)
-                ),
-              ],
-              ts.NodeFlags.Const
-            )
-          );
-        }
-
+        after(ctx, sourceFile);
         return walker(sourceFile, ctx);
       };
     },
   };
+}
+
+export function before(_: ts.TransformationContext, sourceFile: ts.SourceFile, typeChecker: ts.TypeChecker) {
+  // 当前编译文件需要保存的数据
+  const fileData = (METADATA[sourceFile.fileName] = {
+    // 方法定义
+    methodDefine: {},
+    schemaObjects: {},
+  });
+
+  // 遍历文件中所有的 class
+  sourceFile.statements
+    .filter(n => ts.isClassDeclaration(n))
+    .forEach(node => {
+      node.forEachChild(cbNode => {
+        if (isDecoratorNameInclude(cbNode, 'route')) {
+          const clsMethod = getClsMethodKey(
+            (cbNode.parent as any).symbol.escapedName,
+            (cbNode as any).symbol.escapedName
+          );
+
+          const type = typeChecker.getTypeAtLocation(cbNode);
+          const returnType = typeChecker.getReturnTypeOfSignature(
+            type.getCallSignatures()[0]
+          );
+          let realType: SchemaObject;
+          if (getValue(() => returnType.symbol.escapedName) === 'Promise') {
+            realType = getSchemaByType((returnType as any).typeArguments[0], {
+              typeChecker,
+              schemaObjects: fileData.schemaObjects,
+            });
+          } else {
+            realType = getSchemaByType(returnType as ts.ObjectType, {
+              typeChecker,
+              schemaObjects: fileData.schemaObjects,
+            });
+          }
+          if (!fileData.methodDefine[clsMethod]) {
+            fileData.methodDefine[clsMethod] = {
+              paramSchema: {},
+              responseSchema: realType,
+            };
+          }
+        }
+      });
+    });
+}
+
+export function after(_: ts.TransformationContext, sourceFile: ts.SourceFile) {
+  const fileData = METADATA[sourceFile.fileName];
+  if (fileData) {
+    sourceFile.statements.forEach(node => {
+      if (ts.isExpressionStatement(node) && (node as any).expression.arguments) {
+        const className = getValue(
+          () => (node as any).expression.arguments[1].expression.escapedText
+        );
+        const methodName = getValue(() => (node as any).expression.arguments[2].text);
+
+        if (className && methodName) {
+          const responseSchema = getValue(
+            () => fileData.methodDefine[getClsMethodKey(className, methodName)].responseSchema
+          );
+          if (responseSchema) {
+            (node as any).expression.arguments[0].elements = (node as any).expression.arguments[0].elements.concat(
+              ts.createCall((ts as any).getHelperName('__metadata'), undefined, [
+                ts.createLiteral(RESPONSE_SCHEMA_KEY),
+                convert(responseSchema),
+              ]),
+              ts.createCall((ts as any).getHelperName('__metadata'), undefined, [
+                ts.createLiteral(SCHEMA_DEFINITION_KEY),
+                ts.createIdentifier('__SchemaDefinition'),
+              ])
+            );
+          }
+        }
+      }
+    });
+
+    (sourceFile.statements as any).unshift(
+      ts.createVariableDeclarationList(
+        [
+          ts.createVariableDeclaration(
+            '__SchemaDefinition',
+            undefined,
+            convert(fileData.schemaObjects)
+          ),
+        ],
+        ts.NodeFlags.Const
+      )
+    );
+  }
 }
